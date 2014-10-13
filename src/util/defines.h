f68424a4970cbfa6ac2f20d1b252c92d6ca5f91e
Nicolas Cisco <n.cis_92@hotmail.com>
2014-10-13 20:10:59 -0300

    Se agregan comentarios en defines.h

--- src/util/defines.h
+++ src/util/defines.h
@@ -2,29 +2,29 @@
 #define __DEFINES_H__
 #include <unistd.h>
 
-#define SELLER_FIFO "/tmp/seller_fifo"
-#define MONEY_BOX "/tmp/money.box"
-#define MONEY_BOX_CHAR 'A'
-#define QUEUE_FIFO "/tmp/queue_fifo"
-#define KID_FIFO "/tmp/kid_fifo_"
-#define POSICIONES_CALESITA "/tmp/calesita.pos"
-#define POSICIONES_CALESITA_CHAR 'A'
-#define DENTRO_CALESITA_FIFO "/tmp/calesita_dentro"
+#define SELLER_FIFO "/tmp/seller_fifo" // -> Path de archivo fifo que leera el vendedor
+#define MONEY_BOX "/tmp/money.box" // -> Path de SHM para guardar la recaudacion
+#define MONEY_BOX_CHAR 'A' // -> Letra de la SHM donde se guarda la recaudacion
+#define QUEUE_FIFO "/tmp/queue_fifo" // -> Path de archivo fifo que leera la calesita (Cola que hacen los chicos para entrar)
+#define KID_FIFO "/tmp/kid_fifo_" // -> Path de archivo fifo que leen los chicos
+#define POSICIONES_CALESITA "/tmp/calesita.pos" // -> Path de SHM donde se guardan las posiciones de la calesita
+#define POSICIONES_CALESITA_CHAR 'A' // -> Letra de la SHM donde se guardan las posiciones de la calesita
+#define DENTRO_CALESITA_FIFO "/tmp/calesita_dentro" // -> Path de archivo fifo donde escriben los chicos para decir que ya entraron y lee la calesita para saber que ya entraron los chicos
 
-#define SALIDA_LOCK "/tmp/salida.lock"
-#define KIDS_OUT_LOCK "/tmp/kids_out.lock"
+#define SALIDA_LOCK "/tmp/salida.lock" // -> Path de lock file que se usa para que los chicos salgan de la calesita
+#define KIDS_OUT_LOCK "/tmp/kids_out.lock" // -> Path de lock file que se usa para que la calesita sepa cuando ya salieron todos los chivos
 
-#define DEFAULT_TICKET_COST 5
-#define DEFAULT_CANT_ASIENTOS 5
-#define DEFAULT_CANT_CHICOS 8
-#define DEFAULT_TIEMPO_VUELTA 2
-#define DEFAULT_LOG_FILE "./debug.log"
+#define DEFAULT_TICKET_COST 5 // -> Valor por defecto del precio de una entrada
+#define DEFAULT_CANT_ASIENTOS 5 // -> Valor por defecto de la cantidad de asientos de la calesita
+#define DEFAULT_CANT_CHICOS 8 // -> Valor por defecto de la cantidad de chicos en el barrio
+#define DEFAULT_TIEMPO_VUELTA 2 // -> Valor por defecto del tiempo de una vuelta de la calesita
+#define DEFAULT_LOG_FILE "./debug.log" // -> Valor por defecto del archivo de salida en modo DEBUG
 
-#define ENVIROMENT_TICKET_COST "CALESITA_TICKET_COST"
-#define ENVIROMENT_CANT_ASIENTOS "CALESITA_CANT_ASIENTOS"
-#define ENVIROMENT_CANT_CHICOS "CALESITA_CANT_CHICOS"
-#define ENVIROMENT_T_VUELTA "CALESITA_T_VUELTA"
-#define ENVIROMENT_MODE "CALESITA_MODE"
-#define ENVIROMENT_PATH "CALESITA_PATH"
+#define ENVIROMENT_TICKET_COST "CALESITA_TICKET_COST" // -> Nombre de la variable de entorno para el precio de la calesita
+#define ENVIROMENT_CANT_ASIENTOS "CALESITA_CANT_ASIENTOS" // -> Nombre de la variable de entorno para la cantidad de asientos de la calesita
+#define ENVIROMENT_CANT_CHICOS "CALESITA_CANT_CHICOS" // -> Nombre de la variable de entorno para la cantidad de chicos en el barrio
+#define ENVIROMENT_T_VUELTA "CALESITA_T_VUELTA" // -> Nombre de la variable de entorno para el tiempo de una vuelta en la calesita
+#define ENVIROMENT_MODE "CALESITA_MODE" // -> Nombre de la variable de entorno para el modo de la calesita
+#define ENVIROMENT_PATH "CALESITA_PATH" // -> Nombre de la variable de entorno para el archivo de salida en modo DEBUG
 
 #endif
